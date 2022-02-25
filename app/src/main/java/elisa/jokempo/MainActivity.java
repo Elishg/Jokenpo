3b1662e32ccb11683e832608eb468a2fe06e49e5
ELISA SHIGEMORI DOMINGUES <elisa.sdomingues@senacsp.edu.br>
2022-02-25 11:38:04 -0300

    Arquivos jogo

---
+++ app/src/main/java/elisa/jokempo/MainActivity.java
@@ -0,0 +1,88 @@
+package elisa.jokempo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    private ImageView m1;
+    private ImageView m2;
+    private int escolha1 = 0;
+    private int escolha2 = 0;
+    private TextView player1;
+    private TextView player2;
+    private TextView jogar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        m1 = findViewById(R.id.mao1);
+        m2 = findViewById(R.id.mao2);
+        player1 = findViewById(R.id.p1);
+        player2 = findViewById(R.id.p2);
+        jogar = findViewById(R.id.play);
+
+
+        m1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (escolha1 == 0) {
+                    m1.setImageResource(R.drawable.jesus);
+                    escolha1++;
+                } else if (escolha1 == 1) {
+                    m1.setImageResource(R.drawable.papel);
+                    escolha1++;
+                } else if (escolha1 == 2) {
+                    m1.setImageResource(R.drawable.tesoura);
+                    escolha1 = 0;
+                }
+            }
+        });
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                geramao2();
+                verifica();
+            }
+        });
+
+    }
+
+    private void geramao2() {
+        int escolha2 = (int) (Math.random() * 3);
+
+        if (escolha2 == 0) {
+            m2.setImageResource(R.drawable.tesoura);
+        } else if (escolha2 == 1) {
+            m2.setImageResource(R.drawable.jesus);
+        } else if (escolha2 == 2) {
+            m2.setImageResource(R.drawable.papel);
+        }
+
+    }
+
+    private void verifica(){
+        if (escolha1 == escolha2) {
+            jogar.setText("Empatou!!!");
+        }else if (escolha1 == 0 & escolha2 == 2) {
+            jogar.setText("Jogador 1 GANHOU!");
+        }else if (escolha1 == 2 & escolha2 == 0) {
+            jogar.setText("Jogador 2 GANHOU!");
+        }else if(escolha1 > escolha2){
+            jogar.setText("Jogador 1 GANHOU!");
+        }else if(escolha1 < escolha2){
+            jogar.setText("Jogador 2 GANHOU!");
+        }
+    }
+}
---
+++ app/src/main/java/elisa/jokempo/SplashActivity.java
@@ -0,0 +1,31 @@
+package elisa.jokempo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent play = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(play);
+                finish();
+
+            }
+
+        }, 7000);
+    }
+}
